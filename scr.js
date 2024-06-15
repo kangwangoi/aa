b987eeafe3febde21c68df6d54ed2aa1a80f44b0
kangwangoi <153366806+kangwangoi@users.noreply.github.com>
2024-06-15 19:24:20 +0700

    update nav
    update nav1

--- scr.js
+++ scr.js
@@ -1,36 +1,36 @@
-const apiDataMusicURL = "https://www.steamwebapi.com/steam/api/items?key=AMVABEHZ4JS519DG&page=1&max=20&price_min=0&price_max=500000&=0&limit=20&numberOfTopResults=5"
+// const apiDataMusicURL = "https://www.steamwebapi.com/steam/api/items?key=AMVABEHZ4JS519DG&page=1&max=20&price_min=0&price_max=500000&=0&limit=20&numberOfTopResults=5"
 
-const getData = async () => {
-    try {
-        await axios({
-            method: 'get',
-            url: apiDataMusicURL
-          })
-        .then(function (response) {
-            //s
-            const gamesDataArray = response.data
-            console.log(gamesDataArray);
-            const container = document.getElementById("bodycontainer")
-            gamesDataArray.forEach((game) => {
+// const getData = async () => {
+//     try {
+//         await axios({
+//             method: 'get',
+//             url: apiDataMusicURL
+//           })
+//         .then(function (response) {
+//             //s
+//             const gamesDataArray = response.data
+//             console.log(gamesDataArray);
+//             const container = document.getElementById("bodycontainer")
+//             gamesDataArray.forEach((game) => {
 
-                const albumCard = document.createElement('div')
+//                 const albumCard = document.createElement('div')
 
-                albumCard.className = "col-md-2 album-card"
+//                 albumCard.className = "col-md-2 album-card"
 
-                albumCard.innerHTML = `
-                <div class="card">
-                    <img src=${game.itemimage} style="width: 158px; padding: 10px 10px 0 10px;" >
-                    <div class="card-body">
-                    </div>
-                </div>`
+//                 albumCard.innerHTML = `
+//                 <div class="card">
+//                     <img src=${game.itemimage} style="width: 158px; padding: 10px 10px 0 10px;" >
+//                     <div class="card-body">
+//                     </div>
+//                 </div>`
 
-                container.appendChild(albumCard);
-            })
+//                 container.appendChild(albumCard);
+//             })
             
-        });
-    }catch(errors) {
-        console.log(errors);
-    }
-}
-//jh
-getData();  
+//         });
+//     }catch(errors) {
+//         console.log(errors);
+//     }
+// }
+// //jh
+// getData();  
