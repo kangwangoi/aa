b6f87c41320bab94670270c8a8e9d27fb23ed5e9
kangwangoi <153366806+kangwangoi@users.noreply.github.com>
2024-07-03 23:00:41 +0700

    fix bug :not show data

--- scr.js
+++ scr.js
@@ -1,159 +1,125 @@
 
-// const itemPer = 20;
-// let pageBegin = 1;
-// let pageEnd = Math.ceil(91 / itemPer)
-// function pagePre(){
-//     if(currentPage==pageEnd || currentPage>1){
-//         pageBegin--
-//     appendDataElement()
-//     let tam = currentPage
-//     document.getElementById("pageID").innerHTML = `Page ${pageBegin} of ${pageEnd}`  
-// }}
-// function pageNExt(){
-//     if(pageEnd>pageBegin){
-//         pageBegin++
-//     appendDataElement()
-//    let  tam = pageBegin
-//     document.getElementById("pageID").innerHTML = `Page ${pageBegin} of ${pageEnd}`  
-// }}
-// document.getElementById("pageID").innerHTML = `Page ${pageBegin} of ${pageEnd}`  
-// //page number 
-
-
-// const item = []
- document.addEventListener("DOMContentLoaded", ()=>{
-     const apiDataMusicURL = "https://opencritic-api.p.rapidapi.com/game/search?criteria=the%20withcer%203"
-     const fetchApi = async () => {
-        alert("fetching...")
-                        const options = {
-                            method: 'GET',
-                            headers: {
-                                'x-rapidapi-key': '55a8dae2ecmsha374c05867d4de0p168faajsn0555c3e002af',
-                                'x-rapidapi-host': 'opencritic-api.p.rapidapi.com'
-                            }
-                        };
-                        
-                        try {
-                            const response = await fetch(apiDataMusicURL, options);
-                            const result = await response.json();
-                            localStorage.setItem("data", result)
-                            console.log(result);
-                        } catch (error) {
-                            console.error(error);
-                        }
-                    }
-    
-    const appendDataElement = (data) => {
-        // alert("Loading data...")
-        // console.log("data", data)
-        // fetchApi()
-        const container = document.getElementById("gameContainer")
-        console.log(data);
-       
-        data.forEach((game, index) => {
-
+                // const itemPer = 20;
+                // let pageBegin = 1;
+                // let pageEnd = Math.ceil(91 / itemPer); // Example item count for calculation
+                // let currentPage = 1; // Initialize currentPage
             
-                const gameCard = document.createElement('div')
-                gameCard.className = `col-md-2 album-card album-card-${index}`
-                    //price 
-                //     let time = 0;
-                // let price = ""
-                // if (game.priceavg<=0){
-                //     price = "free"
-                // } else {
-                //     price = game.priceavg
+                // function pagePre() {
+                //     if (currentPage > 1) {
+                //         pageBegin--;
+                //         appendDataElement();
+                //         document.getElementById("pageID").innerHTML = `Page ${pageBegin} of ${pageEnd}`;
+                //     }
                 // }
-            //    console.log(typeof(gameCard));
-            //     doi albumCard => gameCard            
-            //     page number begin n end
-                
-            //     console.log(pageBegin);
-            //     for(i=start;i++;i<end){
-            //         gameItem.push(
-            //     gameCard.innerHTML = `
-            //         <div class="card">
-            //             <img src=${item[i].itemimage} style="width: 248px; padding: 10px 10px 0 10px;" />
-            //             <div class="card-body" style ="width:248px">
-            //                 <h5>${game.itemname}</h5>
-            //                 <p>${price} $</p>
-            //             </div>
-            //         </div>
-            //         `
-            //                     )
-            //                         }
-                gameCard.innerHTML`<p>hi</p>`
-                container.appendChild(gameCard);
-            }
-           
-        )
+            
+                // function pageNext() {
+                //     if (pageEnd > pageBegin) {
+                //         pageBegin++;
+                //         appendDataElement();
+                //         document.getElementById("pageID").innerHTML = `Page ${pageBegin} of ${pageEnd}`;
+                //     }
+                // }
+            
+                // document.getElementById("pageID").innerHTML = `Page ${pageBegin} of ${pageEnd}`;
+            
+const urlGame = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': '55a8dae2ecmsha374c05867d4de0p168faajsn0555c3e002af',
+        'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
     }
-//    console.log(tam);
-//    console.log("item",item);
-    const getData = () => {
-        if (JSON.parse(localStorage.getItem("data"))!=null) {
-            alert("success")
-            const data = JSON.parse(localStorage.getItem("data"))
-            appendDataElement(data)
-        }
-        else {
-            fetchApi()
-            alert("bruhhhhhhhhhhhh")
-          const data = JSON.stringify(localStorage.getItem("data"))
-            appendDataElement(data)  
-            console.log(data)   
-        }
+};
+//
+const fetchApi = async () => {
+    try {
+        const response = await fetch(urlGame, options);
+        const result = await response.text();
+        localStorage.setItem("data", JSON.stringify(result));
+        return JSON.parse(result)
+    } catch (error) {
+        console.error(error);
     }
+};
 
-  
-                    
-                    // fetchApi()
-                    //jh
-                    // Khong nen goi ham getData nhieu vi api co gioi han so lan request 
-                    getData()
-                    // Search games
-                    const search = document.getElementById("input_search")
-                    
-                    search.addEventListener("input" , () =>{
-                        const container = document.querySelector("#gameContainer")
-                        const itemsAlbum = container.querySelectorAll(".album-card")
-                        itemsAlbum.forEach(element => { 
-                            element.style.display = 'none';
-                        });
-                        
-                        
-                        const data = JSON.parse(localStorage.getItem("data"))
-                        let name = search.value.trim()
-                        searchItem(name)
-                    })
-                    // fetchApi()
-                    
-                    function searchItem(name){
-                        
-                        if (name != '') {
-                            let isFoundItem = false
-                            const data = JSON.parse(localStorage.getItem("data"))
-                            
-                            data.forEach((game, index) => {
-                                if (game.itemname != null) {
-                                    if (game.itemname.includes(name)){
-                                        document.querySelector(`.album-card-${index}`).style.display = "block"
-                                        isFoundItem = true
-                                    }
-                                }
-                            })
-                            
-                            isFoundItem ? document.getElementById("messageNotFound").innerHTML=``: document.getElementById("messageNotFound").innerHTML = `<div display="text-align: center;">Game not found</div>`
-                        }
-                        else {
-                            const container = document.querySelector("#gameContainer")
-                            const itemsAlbum = container.querySelectorAll(".album-card")
-                            itemsAlbum.forEach(element => { 
-                                element.style.display = 'block';
-                            });
-                        }
-                        
-                    }
+const appendDataElement = (data) => {
+    const container = document.getElementById("gameContainer");
+        const data_arr = JSON.parse(data);
+        data_arr.forEach(game => {
+        const gameCard = document.createElement('div');
+        console.log("data game", game)
+        gameCard.className = `col-md-2 game-card game-card-${game.id}`;
+        console.log("thumbnail: ", game.thumbnail, " - title: ", game.title);
+        gameCard.innerHTML = `
+            <div class="card">
+                <img src=${game.thumbnail} style="width: 248px; padding: 10px 10px 0 10px;"/>
+                <div class="card-body" style="width:248px">
+                    <h5>${game.title || 'N/A'}</h5>
+                    <p>free</p>
+                </div>
+            </div>
+        `;
+        container.appendChild(gameCard);
+    });
+};
+
+const getData = () => {
+let data = localStorage.getItem("data");
+if (data) {
+    appendDataElement(JSON.parse(data));
+} else {
+    data = fetchApi();
+    appendDataElement(data)
+}
+};           
+// fetchApi()
+//jh
+// Khong nen goi ham getData nhieu vi api co gioi han so lan request 
+getData()
+// Search games
+const search = document.getElementById("input_search")
+
+search.addEventListener("input" , () =>{
+    const container = document.querySelector("#gameContainer")
+    const itemsGame = container.querySelectorAll(".game-card")
+    itemsGame.forEach(element => { 
+        element.style.display = 'none';
+    });
+    
+    
+    let name = search.value.trim()
+    searchItem(name)
 })
+// fetchApi() 
+
+function searchItem(name){
+    if (name != '') {
+        let isFoundItem = false
+        const data = JSON.parse(localStorage.getItem("data"))
+        // console.log("data when searching", data)
+        
+        JSON.parse(data).forEach((game) => {
+            if (game.title != null) {
+                if (game.title.includes(name)){
+                    document.querySelector(`.game-card-${game.id}`).style.display = "block";
+                    isFoundItem = true
+                }
+            }
+        })
+        
+        isFoundItem ? document.getElementById("messageNotFound").innerHTML=``: document.getElementById("messageNotFound").innerHTML = `<div display="text-align: center;">Game not found</div>`
+    }
+    else {
+        document.getElementById("messageNotFound").innerHTML='';
+        const container = document.querySelector("#gameContainer")
+        const itemsGame = container.querySelectorAll(".game-card")
+        itemsGame.forEach(element => { 
+            element.style.display = 'block';
+        });
+    }
+    
+}
+
 
 
 
